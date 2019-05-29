bfdf1e765bf3a6cacc7649c1f9f1853d743966be
wasee229 <51176990+wasee229@users.noreply.github.com>
2019-05-30 01:49:00 +0500

    Add files via upload

---
+++ hamy1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta property="og:title" content="Omg Annaconda swallows a man alive :o but what people near him did to annaconda is totally shocking :o "/>
+
+<meta property="og:image" content="http://i.imgsafe.org/bf9963afa8.png"/>
+
+</head>
+
+ <script type='text/javascript'>
+
+   top.location.href = 'https://www.youtube.com/watch?v=30OB5Hh3vQc';
+   
+ </script>
+ <h3>

c4656809af38f45f28f340bad4cb02ddc4fb973b
wasee229 <51176990+wasee229@users.noreply.github.com>
2019-05-30 01:44:25 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta property="og:title" content="Its Time To Make Some Extra Cash"/>
+<meta property="og:image" content="http://www.onlinemoneyexpert.com/wp-content/uploads/2013/06/making-money-from-home-online.jpg"/>
+<body>
+ <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url="http://www.google.com/url?q=http%3A%2F%2Fbit.ly%2F1F7BBPz&sa=D&sntz=1&usg=AFQjCNHtjuswLhr2wly8LZa8A3WAgCPclg=AFQjCNEVe2UZGLgBbZjuRXTk6dotM082xQ">
+        <script type="text/javascript">
+            window.location.href = "http://www.google.com/url?q=http%3A%2F%2Fbit.ly%2F1F7BBPz&sa=D&sntz=1&usg=AFQjCNHtjuswLhr2wly8LZa8A3WAgCPclg"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow the ye<a href='http://example.com'>link to example</a>
+    </body>
+</html>
